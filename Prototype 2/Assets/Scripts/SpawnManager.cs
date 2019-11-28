1863b73a86ccf9d9826870df4d1b43d4e04fe445
Marion Covington <marion.c667@stu.nebo.edu>
2019-11-27 20:21:27 -0700

    Almost finished

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -13,7 +13,7 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomAnimal", startDelay, 1.5f);
+        Invoke ("SpawnRandomAnimal", startDelay, 1.5f);
     }
 
     // Update is called once per frame
